e6a7384a53aed1f3dade8c18eaf0e99dceb3591f
César Claude <cesarclaudepro@gmail.com>
2021-12-03 02:29:58 +0100

    finished ?

--- estimate.py
+++ estimate.py
@@ -1,24 +1,37 @@
 import os
 import csv
+import sys
 
+# Theta file
 filename = './theta.csv'
 
+# Init at 0
 theta0 = 0
 theta1 = 0
 
+# Get theta values from file (if it exists)
 if os.path.isfile(filename):
     with open(filename, newline='') as csvfile:
         spamreader = csv.reader(csvfile)
         for row in spamreader:
             if (row[0] == 'theta0'):
-                theta0 = int(row[1])
+                try:
+                    theta0 = float(row[1])
+                except ValueError:
+                    print(filename, "is invalid.")
+                    sys.exit()
             if (row[0] == 'theta1'):
-                theta1 = int(row[1])
+                try:
+                    theta1 = float(row[1])
+                except ValueError:
+                    print(filename, "is invalid.")
+                    sys.exit()
 
+
+# Loop until user supplies mileage value
 waiting_for_input = True
-
 while (waiting_for_input):
-    mileage = input("Type a mileage to see the estimate price : ")
+    mileage = input("Type a mileage to see the estimated price : ")
 
     try:
         mileage = int(mileage)
@@ -26,4 +39,9 @@ while (waiting_for_input):
     except ValueError:
         print("Please enter an integer !\n")
 
-print(theta0 + (theta1 * mileage))
+# Output estimation based on theta values
+print("\nBased on current predictions, a car with a mileage of", mileage, "kilometers would be worth :")
+print("$", int(theta0 + (theta1 * mileage)))
+
+if (theta0 == 0 and theta1 == 0):
+    print("\n(Without a trained model, estimating won't get us far...)")
--- train.py
+++ train.py
@@ -1,40 +1,47 @@
 import os
 import csv
+import sys
+import matplotlib.pyplot as plt
 
-dataFile = 'data.csv'
-learningRate = 0.01
-nIterations = 10
-
-
+# Goal function
 def estimatePrice (theta0, theta1, mileage):
     return theta0 + (theta1 * mileage)
 
-def mean0 (x, y, theta0, theta1):
+# Theta0 formula : sum of errors
+def meanError0 (x, y, theta0, theta1):
     m = len(x)
     adjustedSum = 0
     for i in range(m):
-        adjustedSum += estimatePrice(theta0, theta1, x[i]) - x[i]
+        adjustedSum += (estimatePrice(theta0, theta1, x[i]) - y[i])
     return adjustedSum / m
 
-def mean1 (x, y, theta0, theta1):
+# Theta1 formula : sum of errors multiplied by x
+def meanError1 (x, y, theta0, theta1):
     m = len(x)
     adjustedSum = 0
     for i in range(m):
-        adjustedSum += (estimatePrice(theta0, theta1, x[i]) - x[i]) * x[i]
-    print(adjustedSum)
+        adjustedSum += (estimatePrice(theta0, theta1, x[i]) - y[i]) * x[i]
     return adjustedSum / m
 
+# Iterating towards local minimum
 def gradientDescent (x, y):
-    theta0 = 1
-    theta1 = 1
+    theta0 = 0
+    theta1 = 0
     for i in range(nIterations):
-        print(theta0, theta1)
-        tmpTheta0 = theta0 - learningRate * mean0(x, y, theta0, theta1)
-        tmpTheta1 = theta1 - learningRate * mean1(x, y, theta0, theta1)
+        tmpTheta0 = theta0 - learningRate * meanError0(x, y, theta0, theta1)
+        tmpTheta1 = theta1 - learningRate * meanError1(x, y, theta0, theta1)
         theta0 = tmpTheta0
         theta1 = tmpTheta1
     return theta0, theta1
 
+# Normalization (all values between 0 and 1) for calculations
+def normalize (lst):
+    normalized = []
+    for elem in lst:
+        normalized.append( (elem - min(lst)) / (max(lst) - min(lst)) )
+    return normalized
+
+# Reading data CSV for x and y values
 def readData ():
     x = []
     y = []
@@ -48,14 +55,55 @@ def readData ():
                 pass
     return x, y
 
+# Saving theta values for estimate.py to use
+def writeData (theta0, theta1):
+    with open(thetaFile, 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+        spamwriter.writerow(['theta0', theta0])
+        spamwriter.writerow(['theta1', theta1])
 
 
+##########################################################################
+##################                  MAIN                  ################
+##########################################################################
+
+# Files
+dataFile = './dazta.csv'
+thetaFile = './theta.csv'
+
+# Constants
+learningRate = 1
+nIterations = 666
+
+
+# Read CSV data
 if os.path.isfile(dataFile):
     x, y = readData()
+    print("Training...")
 else:
     print("Error : no data available")
     sys.exit()
 
-theta0, theta1 = gradientDescent(x, y)
+# Normalize vectors
+normedX = normalize(x)
+normedY = normalize(y)
 
-print(theta0, theta1)
+# Perform the gradient descent
+theta0, theta1 = gradientDescent(normedX, normedY)
+
+# Denormalize theta values
+theta0 = theta0 * (max(y) - min(y)) + min(y)
+theta1 = theta1 * (max(y) - min(y)) / (max(x) - min(x))
+
+# Save theta values
+writeData(theta0, theta1)
+
+print("Done training !")
+print("Data saved at ", thetaFile)
+
+# Display results
+# plt.plot(x, y)
+# plt.plot([min(x), max(x)], [estimatePrice(min(x)), estimatePrice(max(x))])
+# plt.xlabel('Mileage (KM)')
+# plt.ylabel('Price ($)')
+# plt.show()
